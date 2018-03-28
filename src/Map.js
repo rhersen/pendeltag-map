dbb2b1485c814c6a0359235eb8afd33d69f31ce6
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-03-28 07:29:23 +0200

    Extract parameters for zoom

--- src/Map.js
+++ src/Map.js
@@ -6,27 +6,24 @@ import * as classes from './classes'
 export default class Map extends Component {
   render() {
     const { stations, train, back } = this.props
+    const viewBox = '173 0 10 16'
+    const arrow = '174.5 1.5, 173.5 1, 174.5 0.5'
+    const yScale = 14
     return (
-      <svg id="map" className="App-logo" viewBox="173 0 10 16">
+      <svg id="map" className="App-logo" viewBox={viewBox}>
         <g>
-          <polygon
-            onClick={back}
-            className="back"
-            points="174.5 1.5, 173.5 1, 174.5 0.5"
-          />
+          <polygon onClick={back} className="back" points={arrow} />
           {map(stations, (v, k) => (
             <text className="location" key={k} x={x(v)} y={y(v)} fontSize="0.3">
               {k}
             </text>
           ))}
           {map(filters.estimated(train), a => {
-            console.log('estimated', a.ActivityType, a.LocationSignature)
             const v = stations[a.LocationSignature]
             const k = a.ActivityType + a.LocationSignature
             return <circle className="estimated" key={k} cx={x(v)} cy={y(v)} />
           })}
           {map(filters.departed(train), a => {
-            console.log('departed', a.ActivityType, a.LocationSignature)
             const v = stations[a.LocationSignature]
             const k = a.ActivityType + a.LocationSignature
             return (
@@ -39,7 +36,6 @@ export default class Map extends Component {
             )
           })}
           {map(filters.arrived(train), a => {
-            console.log('arrived', a.ActivityType, a.LocationSignature)
             const v = stations[a.LocationSignature]
             const k = a.ActivityType + a.LocationSignature
             return (
@@ -54,13 +50,13 @@ export default class Map extends Component {
         </g>
       </svg>
     )
+
+    function x(v) {
+      return v.east * 10
+    }
+
+    function y(v) {
+      return (60 - v.north) * yScale
+    }
   }
-}
-
-function x(v) {
-  return v.east * 10
-}
-
-function y(v) {
-  return (60 - v.north) * 14
 }
